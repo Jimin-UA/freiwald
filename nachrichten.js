0ff9f956b6f9a2f61335da15ce4e58deb894173e
Jimin-UA <chiminbangtanboys@gmail.com>
2025-08-22 01:16:07 +0300

    Add files via upload

---
+++ nachrichten.js
@@ -0,0 +1,22 @@
+var nachrichten = [
+    {
+        titel: "Der Freistaat Freiwald wurde gegründet!",
+        datum: "20.08.2025"
+    },
+    {
+        titel: "Die Freiwald-Währung wurde eingeführt, die Freiwald-Marke wurde eingeführt",
+        datum: "20.08.2025"
+    },
+    {
+        titel: "Viele Leute waren auf dem Fest \"Freier Wind\".",
+        datum: "18.08.2025"
+    },
+    {
+        titel: "Neue Nachricht: Beispiel-Nachricht mit einem Link.",
+        datum: "17.08.2025"
+    },
+    {
+        titel: "Открыта новая дорога в город Оберндорф.",
+        datum: "16.08.2025"
+    }
+];
